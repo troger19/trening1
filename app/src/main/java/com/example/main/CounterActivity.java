9878026c81a0e9c14b47ebfd56a9b25d2755078f
Jan Babel <jan.babel@ferratum.com>
2015-08-05 08:39:23 +0200

    -

--- app/src/main/java/com/example/main/CounterActivity.java
+++ app/src/main/java/com/example/main/CounterActivity.java
@@ -9,6 +9,7 @@ import android.media.SoundPool;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -19,6 +20,7 @@ import android.widget.TextView;
 
 import com.jano.trening.R;
 
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 @TargetApi(Build.VERSION_CODES.GINGERBREAD)
@@ -27,14 +29,16 @@ public class CounterActivity extends Activity {
 
     private Button btnStart, btnStop;
     private SeekBar seekBarTraining, seekBarPause;
-    private TextView textViewTotalTime, textViewTraining, textViewPause,textViewSeriesTime, textViewPauseTime;
+    private TextView textViewTotalTime, textViewTraining, textViewPause, textViewSeriesTime, textViewPauseTime;
     EditText editTextSeries;
     private SoundPool sounds;
     private int sndtick;
     MediaPlayer mediaPlayer = null;
     String soundTick = "tick";
     CounterClass timer;
-    int totalTrainingTime, seriesTime, pauseTime;
+    int totalTrainingTime, seriesTime, pauseTime, seriesCounter, roundCounter;
+    TextToSpeech t1;
+    boolean switcher = true;
 
 
     @Override
@@ -47,31 +51,33 @@ public class CounterActivity extends Activity {
         textViewTotalTime = (TextView) findViewById(R.id.textViewTotalTime);
         textViewSeriesTime = (TextView) findViewById(R.id.textViewSeriesTime);
         textViewPauseTime = (TextView) findViewById(R.id.textViewPauseTime);
-        textViewTraining= (TextView) findViewById(R.id.textViewTraining);
-        textViewPause= (TextView) findViewById(R.id.textViewPause);
+        textViewTraining = (TextView) findViewById(R.id.textViewTraining);
+        textViewPause = (TextView) findViewById(R.id.textViewPause);
         sounds = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
         sndtick = sounds.load(this, R.raw.ticks10s, 1);
-        seekBarTraining= (SeekBar) findViewById(R.id.seekBarTraining);
-        seekBarPause= (SeekBar) findViewById(R.id.seekBarPause);
-        editTextSeries= (EditText) findViewById(R.id.editTextSeries);
-
+        seekBarTraining = (SeekBar) findViewById(R.id.seekBarTraining);
+        seekBarPause = (SeekBar) findViewById(R.id.seekBarPause);
+        editTextSeries = (EditText) findViewById(R.id.editTextSeries);
 
         btnStart.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 //total training time = (Training + pause) * series
-                totalTrainingTime=(Integer.parseInt(textViewTraining.getText().toString()) + Integer.parseInt(textViewPause.getText().toString()))
+                totalTrainingTime = (Integer.parseInt(textViewTraining.getText().toString()) + Integer.parseInt(textViewPause.getText().toString()))
                         * Integer.parseInt(editTextSeries.getText().toString());
-                seriesTime=Integer.parseInt(textViewTraining.getText().toString());
-                pauseTime=Integer.parseInt(textViewPause.getText().toString());
+                seriesTime = Integer.parseInt(textViewTraining.getText().toString());
+                pauseTime = Integer.parseInt(textViewPause.getText().toString());
                 textViewTotalTime.setText(textViewTraining.getText());
                 textViewSeriesTime.setText(String.valueOf(seekBarTraining.getProgress()));
                 textViewPause.setText(String.valueOf(seekBarPause.getProgress()));
-                if(timer!=null){
+                seriesCounter = Integer.parseInt(editTextSeries.getText().toString());
+                roundCounter = seriesCounter * 2;
+
+                if (timer != null) {
                     timer.cancel();
                 }
-                timer = new CounterClass(totalTrainingTime, 1);
+                timer = new CounterClass(seriesTime, 1);
                 timer.start();
                 managerOfSound(soundTick);
             }
@@ -81,7 +87,7 @@ public class CounterActivity extends Activity {
 
             @Override
             public void onClick(View v) {
-                if(timer!=null){
+                if (timer != null) {
                     timer.cancel();
                 }
                 mediaPlayer.stop();
@@ -122,6 +128,15 @@ public class CounterActivity extends Activity {
             }
         });
 
+        t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
+
     }
 
 
@@ -144,41 +159,47 @@ public class CounterActivity extends Activity {
     @SuppressLint("NewApi")
     public class CounterClass extends CountDownTimer {
 
-        public CounterClass() {
-            super(10000,1000);
-        }
 
         public CounterClass(long secondsInFuture, long countDownInterval) {
-            super(secondsInFuture * 1000, countDownInterval*1000);
+            super(secondsInFuture * 1000, countDownInterval * 1000);
         }
 
 
         @Override
         public void onFinish() {
-            textViewTotalTime.setText("Completed.");
+            Log.i("onFinish ","finish");
+            if (roundCounter > 0) {
+                if (switcher) {
+                    timer = new CounterClass(seriesTime, 1);
+                    Log.i("seriesTime ", String.valueOf(seriesTime));
+                    switcher = !switcher;
+                    timer.start();
+                } else {
+                    timer = new CounterClass(pauseTime, 1);
+                    Log.i("pauseTime ", String.valueOf(pauseTime));
+                    switcher = !switcher;
+                    timer.start();
+                }
+                roundCounter--;
+            }
+            if (roundCounter == 0) {
+                textViewTotalTime.setText("Completed.");
+            }
+
         }
 
         @Override
         public void onTick(long millisUntilFinished) {
             Log.i("milistofinihed = ", String.valueOf(millisUntilFinished / 1000));
             long milis = millisUntilFinished;
-            long seriesMilis = seriesTime*1000;
+
             String hmsTotal = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(milis),
                     TimeUnit.MILLISECONDS.toMinutes(milis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(milis)),
                     TimeUnit.MILLISECONDS.toSeconds(milis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(milis)));
-            Log.i("hmsTotal ",hmsTotal);
+            Log.i("hmsTotal ", hmsTotal);
             textViewTotalTime.setText(hmsTotal);
-
-            String hmsSeries = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(milis),
-                    TimeUnit.MILLISECONDS.toMinutes(milis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(milis)),
-                    TimeUnit.MILLISECONDS.toSeconds(milis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(milis)));
-
-            Log.i("hmsSeries ",hmsSeries);
-            textViewSeriesTime.setText(hmsSeries);
-
-            seriesMilis--;
-
         }
+
 
     }
 
