1feed843fd4abf8beda7b8be043864f524c609e7
Jan Babel <jan.babel@ferratum.com>
2015-08-18 15:13:02 +0200

    -

--- app/src/main/java/com/example/main/MainActivity.java
+++ app/src/main/java/com/example/main/MainActivity.java
@@ -1,49 +1,76 @@
 package com.example.main;
 
-import com.jano.trening.R;
-
-import android.annotation.SuppressLint;
-import android.annotation.TargetApi;
+import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.MenuInflater;
 import android.widget.Button;
 
+import com.jano.trening.R;
 
-@TargetApi(Build.VERSION_CODES.GINGERBREAD)
-@SuppressLint("NewApi")
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+
+
+//@TargetApi(Build.VERSION_CODES.GINGERBREAD)
+//@SuppressLint("NewApi")
+public class MainActivity extends Activity implements ActionBar.OnNavigationListener{
 	
 	private Button btnCounter;
+
+	// action bar
+	private ActionBar actionBar;
+
+	// Title navigation Spinner data
+	private ArrayList<SpinnerNavItem> navSpinner;
+
+	// Navigation adapter
+	private TitleNavigationAdapter adapter;
+
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		btnCounter = (Button) findViewById(R.id.btnCounter);
-		
-		
-		btnCounter.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent("displayCounter_filter");
-				startActivity(intent);
-			}
-		});
+
+		actionBar = getActionBar();
+
+		// Hide the action bar title
+		actionBar.setDisplayShowTitleEnabled(false);
+
+		// Enabling Spinner dropdown navigation
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+
+		// Spinner title navigation data
+		navSpinner = new ArrayList<SpinnerNavItem>();
+		navSpinner.add(new SpinnerNavItem("Local", R.drawable.ic_info_black));
+		navSpinner.add(new SpinnerNavItem("My Places", R.drawable.ic_check_black));
+		navSpinner.add(new SpinnerNavItem("Checkins", R.drawable.ic_alarm_black));
+		navSpinner.add(new SpinnerNavItem("Latitude", R.drawable.ic_accessibility_black));
+
+		// title drop down adapter
+		adapter = new TitleNavigationAdapter(getApplicationContext(), navSpinner);
+
+		// assigning the spinner navigation
+		actionBar.setListNavigationCallbacks(adapter, this);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
 		return true;
+
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.main, menu);
+//		return true;
 	}
 
+	@Override
+	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+		return false;
+	}
 }
