9f287b53e4b2461518846a7f266bcc4ce7ed2cf6
Jan Babel <jan.babel@ferratum.com>
2015-08-19 09:41:52 +0200

    -

--- app/src/main/java/com/example/main/MyListFragment.java
+++ app/src/main/java/com/example/main/MyListFragment.java
@@ -5,46 +5,35 @@ package com.example.main;
  */
 
 
-import android.app.Fragment;
 import android.app.ListFragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.jano.trening.R;
+import com.jano.trening.TrainingConfigurationActivity;
 
 public class MyListFragment extends ListFragment {
+
+    public final static String TRAINING_TYPE = "training_type";
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
                 "Linux", "OS/2" };
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
                 android.R.layout.simple_list_item_1, values);
         setListAdapter(adapter);
     }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
-//        String item = (String) getListAdapter().getItem(position);
-//        Toast.makeText(getActivity(), item + " selected", Toast.LENGTH_LONG).show();
 
-
-        Fragment fragment = new FragmentThree();
-
-        Bundle args = new Bundle();
-
-
-//        fragment.setArguments(args);
-//        FragmentManager frgManager = getFragmentManager();
-//        frgManager.beginTransaction().replace(R.id.content_frame, fragment)
-//                .commit();
-        getChildFragmentManager().beginTransaction().replace(R.id.detailFragment, fragment).addToBackStack(null).commit();
-
-
-
+        Intent intent = new Intent(getActivity(), TrainingConfigurationActivity.class);
+        intent.putExtra(TRAINING_TYPE,getListAdapter().getItem(position).toString());
+        startActivity(intent);
 
     }
 }
---
+++ app/src/main/java/com/jano/trening/TrainingConfigurationActivity.java
@@ -0,0 +1,51 @@
+package com.jano.trening;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import com.example.main.MyListFragment;
+
+public class TrainingConfigurationActivity extends AppCompatActivity {
+
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_training_configuration);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyListFragment.TRAINING_TYPE);
+
+        editText = (EditText) findViewById(R.id.editTextTraining);
+        editText.setText(message);
+
+        System.out.println(message);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_training_configuration, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
