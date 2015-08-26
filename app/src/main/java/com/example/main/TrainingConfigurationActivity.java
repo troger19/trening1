37f058a4ebccc9ca054622b066de84b11b8c5342
Jan Babel <jan.babel@ferratum.com>
2015-08-26 13:16:48 +0200

    -

--- app/src/main/java/com/example/main/TrainingConfigurationActivity.java
+++ app/src/main/java/com/example/main/TrainingConfigurationActivity.java
@@ -1,19 +1,20 @@
 package com.example.main;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 import com.jano.trening.R;
 
-public class TrainingConfigurationActivity extends AppCompatActivity {
 
+public class TrainingConfigurationActivity extends AppCompatActivity {
+/*
     private EditText trainingTime, pauseTime, series;
     private Button btnSave, btnCancel;
     String trainingType;
@@ -76,3 +77,73 @@ public class TrainingConfigurationActivity extends AppCompatActivity {
         series.setText(sharedPref.getString(trainingType + getString(R.string.series), null));
     }
 }
+
+*/
+
+    private LinearLayout mLayout, firstLayout;
+    private Spinner spinner;
+    private Button mButton,btnSave, btnCancel;
+    private Resources res;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_training_configuration);
+        mLayout = (LinearLayout) findViewById(R.id.linearLayoutMain);
+//        firstLayout = (LinearLayout) findViewById(R.id.linearLayout1);
+        firstLayout = (LinearLayout) findViewById(R.id.placeholder);
+        spinner = (Spinner) findViewById(R.id.spinner1);
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(onClick());
+//        btnSave = (Button) findViewById(R.id.btnSave);
+//        btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        res = getResources();
+    }
+
+    private View.OnClickListener onClick() {
+        return new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+//                mLayout.addView(createNewSpinner());
+//                mLayout.addView(createNewLinearLayout());
+                firstLayout.addView(createNewLinearLayout());
+            }
+        };
+    }
+
+    private Spinner createNewSpinner() {
+        final LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        final Spinner spinner = new Spinner(this);
+        String[] items = res.getStringArray(R.array.exercises_arrays);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, items);
+        spinner.setAdapter(adapter);
+        spinner.setLayoutParams(lparams);
+        return spinner;
+    }
+
+    private LinearLayout createNewLinearLayout(){
+        LinearLayout linearLayout = new LinearLayout(this);
+        final LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        final Spinner spinner = new Spinner(this);
+        String[] items = res.getStringArray(R.array.exercises_arrays);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, items);
+        spinner.setAdapter(adapter);
+        spinner.setLayoutParams(lparams);
+//        Button button = new Button(this);
+//        button.setText("fdafd");
+        linearLayout.addView(spinner);
+
+        ViewGroup layout = (ViewGroup) mButton.getParent();
+        if(null!=layout) //for safety only  as you are doing onClick
+            layout.removeView(mButton);
+//        getParent().r
+        linearLayout.addView(mButton);
+
+        return linearLayout;
+
+    }
+}
+
